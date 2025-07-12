794bd6f2a0a29dd1f747e50143213a89b64e434d
Marlo522 <202311932@fit.edu.ph>
2025-07-12 22:52:05 +0800

    feat: implement sign-up functionality and back navigation

--- app/src/main/java/com/example/technovatorsjournal/SignUp.kt
+++ app/src/main/java/com/example/technovatorsjournal/SignUp.kt
@@ -1,20 +1,40 @@
 package com.example.technovatorsjournal
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.technovatorsjournal.databinding.ActivityLoginBinding
+import com.example.technovatorsjournal.databinding.ActivitySignUpBinding
 
 class SignUp : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.btnback.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnSignUp.setOnClickListener {
+            //declare bundle object
+            val bundle = Bundle()
+            bundle.putString("firstname", binding.etFirstName.text.toString())
+            bundle.putString("middlename", binding.etMiddleName.text.toString())
+            bundle.putString("lastname", binding.etLastName.text.toString())
+            bundle.putString("email", binding.etEmail.text.toString())
+            bundle.putString("password", binding.etPassword.text.toString())
         }
     }
 }
